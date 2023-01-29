be79889de11cabe4d6fe9536b7ceda4a1b4f40ed
Abhishek6625 <6625abhi@gmail.com>
2023-01-29 21:03:44 +0530

    file change

--- demo1.py
+++ demo1.py
@@ -1,13 +1,13 @@
 import turtle
-# turtle.bgcolor("Black")
-# squary = turtle.Turtle()
-# squary.speed(1000)
-# for i in range(200):
-#     for color in ["red","blue","green"]:
-#         squary.pencolor(color)
-#         squary.forward(i)
-#         squary.left(80)
-#         squary.right(20)
+turtle.bgcolor("Black")
+squary = turtle.Turtle()
+squary.speed(1000)
+for i in range(200):
+    for color in ["red","blue","green"]:
+        squary.pencolor(color)
+        squary.forward(i)
+        squary.left(80)
+        squary.right(20)
 
 
 
